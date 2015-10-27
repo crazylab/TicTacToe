64861be072da033b6b549fbc58995698b2482cae
rahulna <rahulna@INrahulna.local>
2015-10-27 14:47:00 +0530

    Changed the input of checkDiagonalMatch, checkColumnMatch, checkRowMatch

--- tttLib.js
+++ tttLib.js
@@ -36,40 +36,32 @@ ttt = {
 	}
 }
 //-----------------------------------------------------------------------------
-ttt.getMatrix = function(matrix,move,playerSymbol){
-	move = String(move).split("").map(function(num){
-		return num - 1;
-	});
-	matrix[move[0]][move[1]] = playerSymbol;
-	return matrix;
-}
-//-----------------------------------------------------------------------------
 var matchEverything = function(findMe,insideMe){
 	return findMe.every(function(element){
 		return insideMe.indexOf(element) != -1;
 	});
 }
-ttt.checkDiagonalMatch = function(player){
-	var diagonal1 = matchEverything([11,22,33],player.moves);
-	var diagonal2 = matchEverything([13,22,31],player.moves);
-	return diagonal1 || diagonal2 ? player.symbol : null;
+ttt.checkDiagonalMatch = function(moves){
+	var diagonal1 = matchEverything([11,22,33],moves);
+	var diagonal2 = matchEverything([13,22,31],moves);
+	return diagonal1 || diagonal2;
 }
-ttt.checkColumnMatch = function(player){
-	var column1 = matchEverything([11,21,31],player.moves);
-	var column2 = matchEverything([12,22,32],player.moves);
-	var column3 = matchEverything([13,23,33],player.moves);
-	return column1 || column2 || column3 ? player.symbol : null;
+ttt.checkColumnMatch = function(moves){
+	var column1 = matchEverything([11,21,31],moves);
+	var column2 = matchEverything([12,22,32],moves);
+	var column3 = matchEverything([13,23,33],moves);
+	return column1 || column2 || column3;
 }
-ttt.checkRowMatch = function(player){
-	var row1 = matchEverything([11,12,13],player.moves);
-	var row2 = matchEverything([21,22,23],player.moves);
-	var row3 = matchEverything([31,32,33],player.moves);
-	return row1 || row2 || row3 ? player.symbol : null;
+ttt.checkRowMatch = function(moves){
+	var row1 = matchEverything([11,12,13],moves);
+	var row2 = matchEverything([21,22,23],moves);
+	var row3 = matchEverything([31,32,33],moves);
+	return row1 || row2 || row3;
 }
 ttt.checkForMatch = function(player){
-	var diagonal = ttt.checkDiagonalMatch(player);
-	var row = ttt.checkRowMatch(player);
-	var column = ttt.checkColumnMatch(player);
+	var diagonal = ttt.checkDiagonalMatch(player.moves);
+	var row = ttt.checkRowMatch(player.moves);
+	var column = ttt.checkColumnMatch(player.moves);
 	return diagonal || row || column ? player.symbol : null;
 }
 //-----------------------------------------------------------------------------
--- tttTest.js
+++ tttTest.js
@@ -48,65 +48,24 @@ test["setMatrix sets winner symbol in case of win condition"] = function(){
 	m.setMatrix = {symbol: 'X', moves:[33,22,11]};
 	assert.equal('X',m.winner);
 }
-test["getMatrix gives an 2D array with effect of user move"]=function(){
-	var matrix=  [ 	['','',''],
-					['','',''],
-					['','','']];
-	var move=11;
-	var playerSymbol='O';
-	var expected=  [['O','',''],
-					['','',''],
-					['','','']];
-	assert.deepEqual(expected,m.getMatrix(matrix,move,playerSymbol));
-};
-test["checkDiagonalMatch gives player symbol when diagonaly all symbol matches started from 00 position"]=function(){
-	var playerO={symbol:'O',
-				 moves:[11,21,22,33]};
-	var expected='O';
-	assert.deepEqual(expected,m.checkDiagonalMatch(playerO));
-}
 test["checkDiagonalMatch gives player symbol when it matches diagonaly during forwar diagonal match"]=function(){
-	var playerO={symbol:'O',
-				 moves:[33,13,31,22]};
-	var expected='O';
-	assert.deepEqual(expected,m.checkDiagonalMatch(playerO));
+	assert.deepEqual(true,m.checkDiagonalMatch([33,13,31,22]));
 }
-test["checkDiagonalMatch gives player symbol when it matches diagonaly "]=function(){
-	var playerO={symbol:'O',
-				 moves:[11,22,23,32,33]};
-	var expected='O';
-	assert.deepEqual(expected,m.checkDiagonalMatch(playerO));
+test["checkDiagonalMatch gives player symbol when it matches backward diagonaly "]=function(){
+	assert.deepEqual(true,m.checkDiagonalMatch([11,22,23,32,33]));
 }
 test["checkColumnMatch gives player symbol when all the symbol of one of the column matches"]=function(){
-	var playerX={symbol:'X',
-				 moves:[21,33,23,12,13]};
-	var expected='X'
-	assert.deepEqual(expected,m.checkColumnMatch(playerX));
-}
-test["checkColumnMatch gives player symbol when all the symbol of one column matches and the full game is not complete but O wins"]=function(){
-	var playerO={symbol:'O',
-				 moves:[22,12,32]};
-	var expected='O';
-	assert.deepEqual(expected,m.checkColumnMatch(playerO));
-}
-test["checkColumnMatch gives player symbol when all the symbol of one column matches and the full game is not complete but X wins"]=function(){
-	var playerX={symbol:'X',
-				 moves:[11,21,31]};
-	var expected='X';
-	assert.deepEqual(expected,m.checkColumnMatch(playerX));
+	assert.deepEqual(true,m.checkColumnMatch([11,21,31]));
+	assert.deepEqual(true,m.checkColumnMatch([22,12,32]));
+	assert.deepEqual(true,m.checkColumnMatch([13,23,33]));
+	assert.deepEqual(false,m.checkColumnMatch([11,22,12,13]));
 }
 test["checkRowMatch gives player symbol when all the symbol of one of the row matches"]=function(){
-	var playerX={symbol:'X',
-				 moves:[33,12,31,32,23]};
-	var expected='X'
-	assert.deepEqual(expected,m.checkRowMatch(playerX));
+	assert.deepEqual(true,m.checkRowMatch([11,12,13]));
+	assert.deepEqual(true,m.checkRowMatch([21,22,23]));
+	assert.deepEqual(true,m.checkRowMatch([31,32,33]));
+	assert.deepEqual(false,m.checkRowMatch([11,23,31,32,23]));
 }
-test["checkForMatch checks whether a player had a match in row or column or diagonal and gives the winning player symbol when match is in diagonal"]=function(){
-	var playerO={symbol:'O',
-				 moves:[11,22,33]};
-	var expected='O';
-	assert.deepEqual(expected,m.checkForMatch(playerO));
-};
 test["checkForMatch checks whether a player had a match in row or column or diagonal and gives null when there is no match"]=function(){
 	var playerO={symbol:'O',
 				 moves:[21,33,22,12,31]};
